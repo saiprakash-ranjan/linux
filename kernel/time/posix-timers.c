2fe4470615d2a0c7f1b5aa9d60f11bab4c2056d8
Guenter Roeck <groeck@chromium.org>
2018-09-21 09:33:24 -0700

    CHROMIUM: alt-syscall: Make required syscalls available for use
    Syscalls required by alt-syscall can not be called directly. Unless syscalls
    are already made available for kernel use, create the necessary functions
    as well as wrappers for the real syscalls.
    
    BUG=chromium:885330
    TEST=Build and run on any target or in qemu
    
    Change-Id: Ia0846a1e2a8c168c75046e24fd170d8a952cb46d
    Signed-off-by: Guenter Roeck <groeck@chromium.org>

--- kernel/time/posix-timers.c
+++ kernel/time/posix-timers.c
@@ -1052,8 +1052,7 @@ int do_clock_adjtime(const clockid_t which_clock, struct __kernel_timex * ktx)
 	return kc->clock_adj(which_clock, ktx);
 }
 
-SYSCALL_DEFINE2(clock_adjtime, const clockid_t, which_clock,
-		struct __kernel_timex __user *, utx)
+int ksys_clock_adjtime(const clockid_t which_clock, struct __kernel_timex __user * utx)
 {
 	struct __kernel_timex ktx;
 	int err;
@@ -1067,6 +1066,12 @@ SYSCALL_DEFINE2(clock_adjtime, const clockid_t, which_clock,
 		return -EFAULT;
 
 	return err;
+}
+
+SYSCALL_DEFINE2(clock_adjtime, const clockid_t, which_clock,
+		struct __kernel_timex __user *, utx)
+{
+	return ksys_clock_adjtime(which_clock, utx);
 }
 
 SYSCALL_DEFINE2(clock_getres, const clockid_t, which_clock,
@@ -1122,8 +1127,7 @@ SYSCALL_DEFINE2(clock_gettime32, clockid_t, which_clock,
 	return err;
 }
 
-SYSCALL_DEFINE2(clock_adjtime32, clockid_t, which_clock,
-		struct old_timex32 __user *, utp)
+int ksys_clock_adjtime32(clockid_t which_clock, struct old_timex32 __user * utp)
 {
 	struct __kernel_timex ktx;
 	int err;
@@ -1138,6 +1142,12 @@ SYSCALL_DEFINE2(clock_adjtime32, clockid_t, which_clock,
 		err = put_old_timex32(utp, &ktx);
 
 	return err;
+}
+
+SYSCALL_DEFINE2(clock_adjtime32, clockid_t, which_clock,
+		struct old_timex32 __user *, utp)
+{
+	return ksys_clock_adjtime32(which_clock, utp);
 }
 
 SYSCALL_DEFINE2(clock_getres_time32, clockid_t, which_clock,
