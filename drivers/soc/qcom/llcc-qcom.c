88b7c1b0bcb0275703ef2bc4222187e8e2357440
Sai Prakash Ranjan <saiprakash.ranjan@codeaurora.org>
2020-02-17 15:37:37 +0530

    llcc perfmon
    Change-Id: I5bdabc78e3ad22343adc4799f08fe34774cb904a
    Signed-off-by: Sai Prakash Ranjan <saiprakash.ranjan@codeaurora.org>

--- drivers/soc/qcom/llcc-qcom.c
+++ drivers/soc/qcom/llcc-qcom.c
@@ -413,7 +413,7 @@ static int qcom_llcc_probe(struct platform_device *pdev)
 	u32 num_banks;
 	struct device *dev = &pdev->dev;
 	int ret, i;
-	struct platform_device *llcc_edac;
+	struct platform_device *llcc_edac, *llcc_perfmon;
 	const struct qcom_llcc_config *cfg;
 	const struct llcc_slice_config *llcc_cfg;
 	u32 sz;
@@ -489,6 +489,12 @@ static int qcom_llcc_probe(struct platform_device *pdev)
 		if (IS_ERR(llcc_edac))
 			dev_err(dev, "Failed to register llcc edac driver\n");
 	}
+
+	llcc_perfmon = platform_device_register_data(&pdev->dev,
+					"qcom_llcc_perfmon", -1,
+					drv_data, sizeof(*drv_data));
+	if (IS_ERR(llcc_perfmon))
+		dev_err(dev, "Failed to register llcc perfmon device\n");
 
 	return 0;
 err:
